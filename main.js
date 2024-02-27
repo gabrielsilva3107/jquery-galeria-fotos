b473f871ed16a246b20c5fda1f9c6ba2771c3b1b
gabrielsilva3107 <155502091+gabrielsilva3107@users.noreply.github.com>
2024-02-27 00:08:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
 $(document).ready(function() {
-    $('header button').click(function() {
-        $('form').slideDown();
-    })
-
-    $('#botao-cancelar').click(function() {
-        $('form').slideUp();
-    })
-
-    $('form').on('submit', function(e) {
+    $('#form-tarefa').submit(function(e) {
         e.preventDefault();
-        const endereçoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${endereçoDaNovaImagem}" />`).appendTo(novoItem);
-        $(`
-            <div class="overlay-imagem-link">
-                <a href="${endereçoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                Ver imagem em tamanho real
-                </a>
-            </div>
-        `).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1000);
-        $('#endereco-imagem-nova').val('');
-    })
-})
+        const nomeTarefa = $('#nome-tarefa').val();
+        if (nomeTarefa.trim() !== '') {
+            const novaTarefa = $('<li></li>').text(nomeTarefa);
+            $('#lista-tarefas').append(novaTarefa);
+            $('#nome-tarefa').val('');
+        }
+    });
+    
+    $('#lista-tarefas').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
